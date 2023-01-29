ff144466cc43271f3e4a940b03f48803bb369bc9
IHannes <hannes.hildner@icloud.com>
2023-01-29 13:51:32 +0100

    program to sort words alphabetically

---
+++ sorter.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class sorter {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+        while(!(al.contains("Stop") || al.contains("stop"))){
+        String str = "Gebe Stop ein wenn du alle zu sortierenden Begriffe eingegeben hast\n";
+        str+="Die bisherige Eingabe ist: " + al.toString(); 
+        al.add(JOptionPane.showInputDialog(null, str).toLowerCase());
+        }
+        Collections.sort(al);
+        String str = new String();
+        for(int i = 0; i<al.size(); i++){
+            str+=al.get(i)+"\n";
+        }
+        JOptionPane.showMessageDialog(null, "Die Sortierte Liste ist:\n" + str);
+    }
+}
